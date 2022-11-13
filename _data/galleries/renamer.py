3497e8490d28816273425eb2fe626a95e964833b
sajdoann <sajdoann@fit.cvut.cz>
2022-11-13 21:50:21 +0800

    10 captions

--- _data/galleries/renamer.py
+++ _data/galleries/renamer.py
@@ -13,7 +13,7 @@ from os.path import isfile, join
 # configuration
 output_file = "tmp.yml"
 input_file = output_file
-image_path = "10/gallery-1"
+image_path = "10/aa"#"10/gallery-1"
 extensions= ['jpg', 'png']
 
 # set correct path
